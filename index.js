9efd8a7c87e8944e802ae4f2eca4fb355cc0485a
Diniden <diniden@outlook.com>
2019-11-23 19:48:42 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const postHTML = require("posthtml");
+const posthtmlInlineAssets = require("posthtml-inline-assets");
+
+module.exports = bundler => {
+  bundler.on("bundled", (bundle) => {
+    const bundles = Array.from(bundle.childBundles).concat([bundle]);
+    return Promise.all(bundles.map(async bundle => {
+      if (!bundle.entryAsset || bundle.entryAsset.type !== "html") return;
+
+      const cwd = bundle.entryAsset.options.outDir;
+      const data = fs.readFileSync(bundle.name);
+      const result = await postHTML([posthtmlInlineAssets({ cwd })]).process(data);
+      fs.writeFileSync(bundle.name, result.html);
+    }));
+  });
+};
