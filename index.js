06c23be40f72c4f790f59e831a81ead090252299
Diniden <diniden@outlook.com>
2019-11-23 19:56:58 -0700

    Added to config for the extensions to apply this to

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const fs = require("fs");
 const postHTML = require("posthtml");
 const posthtmlInlineAssets = require("posthtml-inline-assets");
+const package = require(
+  require('app-root-path').resolve('/package.json')
+);
 
 module.exports = bundler => {
   bundler.on("bundled", (bundle) => {
     const bundles = Array.from(bundle.childBundles).concat([bundle]);
+    const extensions = (package.inliner && Array.isArray(package.inliner.ext)) || [];
+
     return Promise.all(bundles.map(async bundle => {
-      if (!bundle.entryAsset || bundle.entryAsset.type !== "html") return;
+      if (!bundle.entryAsset || extensions.indexOf(bundle.entryAsset.type) >= 0) return;
 
       const cwd = bundle.entryAsset.options.outDir;
       const data = fs.readFileSync(bundle.name);
